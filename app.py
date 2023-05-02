cdedd0ff3a70c516d30562c7d685d1a38f9eff18
sihombing18 <126742581+sihombing18@users.noreply.github.com>
2023-05-02 11:18:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_('index.html')
 
 @app.route('/diary', methods=['GET'])
 def show_diary():
